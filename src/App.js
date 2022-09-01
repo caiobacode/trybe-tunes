a118b82187abb464f62558ea56bf987e40fc4b7d
caiobacode <caioodsf@gmail.com>
2022-09-01 18:30:51 -0300

    requisito1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+import Login from './services/pages/login';
+import Search from './services/pages/Search';
+import Album from './services/pages/Album';
+import Favorites from './services/pages/Favorites';
+import Profile from './services/pages/profile';
+import ProfileEdit from './services/pages/profileEdit';
+import NotFound from './services/pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTusdfges</p>);
+    return (
+    <div>
+      <p>TrybeTunes</p>;
+      <Route path='/' component={Login} />
+      <Route path='/search' component={Search} />
+      <Route path='/album/:id' component={Album} />
+      <Route path='/favorites' component={Favorites} />
+      <Route path='/profile' component={Profile} />
+      <Route path='/profile/edit/' component={ProfileEdit} />
+      <Route component={NotFound} />
+    </div>
+    )
   }
 }
 
