27af8bcc587607b4b361143d7c4b2e203f17edff
caiobacode <caioodsf@gmail.com>
2022-09-02 18:23:50 -0300

    requisito 5

--- src/services/pages/search.js
+++ src/services/pages/search.js
@@ -2,11 +2,48 @@ import React from 'react';
 import Header from '../../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artist: '',
+    searchButton: true,
+  };
+
+  disabled = () => {
+    const { artist } = this.state;
+    const min = 2;
+    if (artist.length >= min) {
+      this.setState({ searchButton: false });
+    } else {
+      this.setState({ searchButton: true });
+    }
+  };
+
+  handleChange = async (event) => {
+    const { target } = event;
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      this.disabled();
+    });
+  };
+
   render() {
+    const { searchButton, artist } = this.state;
     return (
       <div>
         <div data-testid="page-search">
           <Header />
+          <input
+            data-testid="search-artist-input"
+            value={ artist }
+            name="artist"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ searchButton }
+          >
+            Pesquisar
+          </button>
         </div>
       </div>
     );
