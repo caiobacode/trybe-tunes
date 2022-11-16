11560b8c12ec05a08aef7f80feeb9cee18a666ef
caiobacode <caioodsf@gmail.com>
2022-11-15 21:22:17 -0300

    100%

--- src/services/pages/profileEdit.js
+++ src/services/pages/profileEdit.js
@@ -1,6 +1,7 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import Header from '../../components/Header';
-import { getUser } from '../userAPI';
+import { getUser, updateUser } from '../userAPI';
 import Carregando from './carregando';
 
 class ProfileEdit extends React.Component {
@@ -10,28 +11,58 @@ class ProfileEdit extends React.Component {
     image: '',
     description: '',
     loading: false,
+    disabledBtn: true,
   };
 
   async componentDidMount() {
     this.setState({ loading: true });
     const actualUser = await getUser();
     this.setState({ ...actualUser,
-      loading: false });
+      loading: false }, () => {
+      this.handleDisableBtn();
+    });
   }
 
+  handleDisableBtn = () => {
+    const maxLength = 4;
+    const stateFiltered = Object.values(this.state)
+      .filter((i) => i !== '' && i !== true && i !== false);
+    const { email } = this.state;
+    const regex = /\S+@\S+\.\S+/;
+    const verifyEmail = email && regex.test(email);
+    if (verifyEmail && stateFiltered.length === maxLength) {
+      this.setState({ disabledBtn: false });
+      return;
+    }
+    this.setState({ disabledBtn: true });
+  };
+
   handleChange = async (event) => {
-    console.log(this.state);
     const { target } = event;
     const { name, value } = target;
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, () => {
+      this.handleDisableBtn();
+    });
   };
 
   handleSubmit = () => {
-
+    const { history } = this.props;
+    const { name, email, image, description } = this.state;
+    const newObj = {
+      name,
+      email,
+      image,
+      description,
+    };
+    this.setState({ loading: true }, async () => {
+      await updateUser(newObj);
+      this.setState({ loading: false });
+      history.push('/profile');
+    });
   };
 
   render() {
-    const { name, image, description, email, loading } = this.state;
+    const { name, image, description, email, loading, disabledBtn } = this.state;
     return (
       <div>
         <Header />
@@ -40,14 +71,38 @@ class ProfileEdit extends React.Component {
             : (
               <div data-testid="page-profile-edit">
                 <input
+                  data-testid="edit-input-name"
                   name="name"
                   type="text"
                   value={ name }
                   onChange={ this.handleChange }
                 />
+                <input
+                  data-testid="edit-input-email"
+                  name="email"
+                  type="text"
+                  value={ email }
+                  onChange={ this.handleChange }
+                />
+                <input
+                  data-testid="edit-input-image"
+                  name="image"
+                  type="text"
+                  value={ image }
+                  onChange={ this.handleChange }
+                />
+                <input
+                  data-testid="edit-input-description"
+                  name="description"
+                  type="text"
+                  value={ description }
+                  onChange={ this.handleChange }
+                />
                 <button
+                  data-testid="edit-button-save"
                   type="button"
                   onClick={ this.handleSubmit }
+                  disabled={ disabledBtn }
                 >
                   Submit
 
@@ -59,5 +114,9 @@ class ProfileEdit extends React.Component {
     );
   }
 }
+
+ProfileEdit.propTypes = {
+  history: PropTypes.shape.isRequired,
+};
 
 export default ProfileEdit;
